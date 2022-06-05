82056e889a9c7c3e9d233e3c0fc847a6548f1ec3
Seungwon Min <31559278+zxcv551133@users.noreply.github.com>
2022-06-05 16:23:46 +0900

    Time: 672 ms (37.82%), Space: 71.3 MB (51.62%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.kt
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.kt
@@ -1,5 +1,18 @@
 class Solution {
     fun sortedSquares(nums: IntArray): IntArray {
-        return nums.map { it * it }.sorted().toIntArray()
+        val numsRet = Array(nums.size) { 0 }
+        var hi = nums.size - 1
+        var lo = 0
+        nums.indices.reversed().forEach { idx ->
+            if (kotlin.math.abs(nums[lo]) > kotlin.math.abs(nums[hi])) {
+                numsRet[idx] = nums[lo] * nums[lo]
+                lo++
+            } else {
+                numsRet[idx] = nums[hi] * nums[hi]
+                hi--
+            }
+        }
+        
+        return numsRet.toIntArray()
     }
 }
