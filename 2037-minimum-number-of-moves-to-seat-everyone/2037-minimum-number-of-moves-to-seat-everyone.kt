bf34a0c76a38f311e6a73692e6c162329f4d7522
Seungwon Min <31559278+zxcv551133@users.noreply.github.com>
2022-07-23 23:24:55 +0900

    Time: 216 ms (87.50%), Space: 38.6 MB (100.00%) - LeetHub

--- 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.kt
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.kt
@@ -1,9 +1,9 @@
 class Solution {
     fun minMovesToSeat(seats: IntArray, students: IntArray): Int {
-        val sortedSeats = seats.sorted()
-        val sortedStudents = students.sorted()
-        return sortedSeats.zip(sortedStudents).map { (seats, students) ->
-            kotlin.math.abs(seats - students)
-        }.sum()
+        seats.sort()
+        students.sort()
+        return seats.zip(students).fold(0) { acc, (seat, student) ->
+            acc + kotlin.math.abs(seat - student)
+        }
     }
 }
