cb17f7e695864ebf505e7513e51486a6bb7f2d7b
Seungwon Min <31559278+zxcv551133@users.noreply.github.com>
2022-08-25 22:13:28 +0900

    Time: 420 ms (61.30%), Space: 44.9 MB (83.68%) - LeetHub

--- 383-ransom-note/383-ransom-note.kt
+++ 383-ransom-note/383-ransom-note.kt
@@ -1,5 +1,5 @@
 class Solution {
-fun canConstruct(ransomNote: String, magazine: String): Boolean = IntArray(26).apply {
+    fun canConstruct(ransomNote: String, magazine: String): Boolean = IntArray(26).apply {
         magazine.forEach { c -> this[c - 'a']++ }
         ransomNote.forEach { c ->
             if (--this[c - 'a'] < 0) return false
