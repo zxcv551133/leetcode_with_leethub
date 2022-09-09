16cb7309b9fa4044c8392ca8faad1dc7b1f5653e
Seungwon Min <31559278+zxcv551133@users.noreply.github.com>
2022-09-09 23:37:20 +0900

    Time: 368 ms (26.08%), Space: 39.8 MB (35.89%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.kt
@@ -0,0 +1,28 @@
+class Solution {
+    class Memoize<T, R>(val func: (T) -> R) {
+        private val cache = mutableMapOf<T, R>()
+        operator fun getValue(thisRef: Any?, property: kotlin.reflect.KProperty<*>) = { n: T ->
+            cache.getOrPut(n) { func(n) }
+        }
+    }
+    
+    fun generateParenthesis(n: Int): List<String> {
+        return makeParenthesis(n)
+    }
+
+    val makeParenthesis: (Int) -> List<String> by Memoize { n ->
+        when (n) {
+            0 -> listOf("")
+            1 -> listOf("()")
+            else -> {
+                val strList = (0 until n).flatMap { idx ->
+                    val rightStrList = generateParenthesis(n - idx - 1)
+                    val leftStrList = generateParenthesis(idx).map { "($it)" }
+                    val conStrList = leftStrList.flatMap { leftStr -> rightStrList.map { leftStr + it } }
+                    conStrList
+                }
+                strList
+            }
+        }
+    }
+}
