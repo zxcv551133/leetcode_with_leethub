473cd68de7280362fd0902b7d6701ec9b117ae95
Seungwon Min <31559278+zxcv551133@users.noreply.github.com>
2022-09-27 06:37:04 +0900

    Time: 331 ms (5.55%), Space: 40.6 MB (5.56%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.kt
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.kt
@@ -1,6 +1,7 @@
 class Solution {
-    private val mutableMap = mutableMapOf<String, String>()
-    private fun findParent(c: String): String {
+    private val mutableMap = mutableMapOf<Char, Char>()
+    
+    private fun findParent(c: Char): Char {
         if (mutableMap[c] == null) {
             mutableMap[c] = c
         }
@@ -8,7 +9,7 @@ class Solution {
         else findParent(mutableMap[c]!!)
     }
 
-    private fun unionParent(c1: String, c2: String) {
+    private fun unionParent(c1: Char, c2: Char) {
         val p1 = findParent(c1)
         val p2 = findParent(c2)
         if (p1 != p2) {
@@ -23,16 +24,16 @@ class Solution {
     fun equationsPossible(equations: Array<String>): Boolean {
         val equals = equations.filter { it.substring(1, 3) == "==" }
         val notEquals = equations.filter { it.substring(1, 3) == "!=" }
-        
+
         equals.forEach {
-            val c1 = it.substringBefore("==")
-            val c2 = it.substringAfter("==")
+            val c1 = it[0]
+            val c2 = it[3]
             unionParent(c1, c2)
         }
 
         notEquals.forEach {
-            val c1 = it.substringBefore("!=")
-            val c2 = it.substringAfter("!=")
+            val c1 = it[0]
+            val c2 = it[3]
             if (findParent(c1) == findParent(c2)) return false
         }
         return true
