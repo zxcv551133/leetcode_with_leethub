9eebc523bb37dc7013f2def52a220ea751e29839
Seungwon Min <31559278+zxcv551133@users.noreply.github.com>
2022-10-03 14:50:01 +0900

    Time: 597 ms (12.50%), Space: 71.9 MB (12.50%) - LeetHub

--- 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.kt
+++ 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.kt
@@ -6,8 +6,6 @@ class Solution {
         }.toIntArray()
         
         return (0..s.length).map { idx ->
-//            preSum[idx] 란 0부터 idx - 1 까지의 1의 개수
-//            preSum[s.length] - preSum[idx] 란 끝에서부터 idx 까지의 1의 개수 이건 유지
             preSum[idx] + s.length - idx - preSum[s.length] + preSum[idx]
         }.min()!!
     }
