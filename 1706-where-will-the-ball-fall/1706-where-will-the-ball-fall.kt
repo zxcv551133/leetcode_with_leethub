161bae0a990f714de50b2ad2027291db850496db
Seungwon Min <31559278+zxcv551133@users.noreply.github.com>
2022-11-02 02:08:38 +0900

    Time: 540 ms (32.73%), Space: 53 MB (74.55%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.kt
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.kt
@@ -1,15 +1,15 @@
 class Solution {
-        fun findBall(A: Array<IntArray>): IntArray {
-            fun drop(i: Int, j: Int): Int {
-                return if (i == A.size) {
-                    j
-                } else if (j in 0 until A[0].size && j + A[i][j] in 0 until A[0].size && A[i][j] == A[i][j + A[i][j]]) {
-                    drop(
-                        i + 1,
-                        j + A[i][j]
-                    )
-                } else -1
-            }
-            return IntArray(A[0].size) { drop(0, it) }
+    fun findBall(A: Array<IntArray>): IntArray {
+        fun drop(i: Int, j: Int): Int {
+            return if (i == A.size) {
+                j
+            } else if (j in 0 until A[0].size && j + A[i][j] in 0 until A[0].size && A[i][j] == A[i][j + A[i][j]]) {
+                drop(
+                    i + 1,
+                    j + A[i][j]
+                )
+            } else -1
         }
+        return IntArray(A[0].size) { drop(0, it) }
+    }
 }
