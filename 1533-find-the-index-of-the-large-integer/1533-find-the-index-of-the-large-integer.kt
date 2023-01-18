55d7b6af7c7766edd66244607df3468225459236
Seungwon Min <31559278+zxcv551133@users.noreply.github.com>
2023-01-18 22:15:20 +0900

    Time: 585 ms (100.00%), Space: 89.1 MB (100.00%) - LeetHub

---
+++ 1533-find-the-index-of-the-large-integer/1533-find-the-index-of-the-large-integer.kt
@@ -0,0 +1,48 @@
+/**
+ * // This is ArrayReader's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface ArrayReader {
+ *     // Compares the sum of arr[l..r] with the sum of arr[x..y] 
+ *     // return 1 if sum(arr[l..r]) > sum(arr[x..y])
+ *     // return 0 if sum(arr[l..r]) == sum(arr[x..y])
+ *     // return -1 if sum(arr[l..r]) < sum(arr[x..y])
+ *     fun compareSub(l: Int, r: Int, x: Int, y: Int): Int {}
+ *
+ *     // Returns the length of the array
+ *     fun length(): Int {}
+ * }
+ */
+
+class Solution {
+    fun getIndex(reader: ArrayReader): Int {
+        val arrayLength = reader.length()
+        fun optimistic(leftSt: Int, leftEd: Int, rightSt: Int, rightEd: Int): Int {
+            println("$leftSt, $leftEd, $rightSt, $rightEd")
+            if (rightEd - leftSt == 1) {
+                return when (reader.compareSub(leftSt, leftEd, rightSt, rightEd)) {
+                    1 -> leftSt
+                    -1 -> rightEd
+                    else -> -1
+                }
+            } else if (rightEd - leftSt == 2) {
+                val opt = optimistic(leftSt, leftEd, rightSt, rightEd - 1)
+                return if (opt == -1) rightEd else opt
+            } else if ((rightEd - leftSt) % 2 == 0) {
+                return when (reader.compareSub(leftSt, leftEd, rightSt, rightEd - 1)) {
+                    0 -> rightEd
+                    1 -> optimistic(leftSt, (leftSt + leftEd - 1) / 2, (leftSt + leftEd - 1) / 2 + 1, leftEd)
+                    -1 -> optimistic(rightSt, (rightSt + rightEd - 1) / 2, (rightSt + rightEd - 1) / 2 + 1, rightEd)
+                    else -> -1
+                }
+            } else {
+                return when (reader.compareSub(leftSt, leftEd, rightSt, rightEd)) {
+                    1 -> optimistic(leftSt, (leftSt + leftEd - 1) / 2, (leftSt + leftEd - 1) / 2 + 1, leftEd)
+                    -1 -> optimistic(rightSt, (rightSt + rightEd - 1) / 2, (rightSt + rightEd - 1) / 2 + 1, rightEd)
+                    else -> -1
+                }
+            }
+        }
+        return if (arrayLength == 1) 1
+        else optimistic(0, arrayLength / 2 - 1, arrayLength / 2, arrayLength - 1)
+    }
+}
