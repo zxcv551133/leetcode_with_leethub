42cbd937ed3c125a29dcccb4b73b90bbd38261de
Seungwon Min <31559278+zxcv551133@users.noreply.github.com>
2023-02-02 11:14:37 +0900

    Time: 245 ms (28.57%), Space: 38.3 MB (9.52%) - LeetHub

---
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun isAlienSorted(words: Array<String>, order: String): Boolean {
+        val orderMap = order.mapIndexed { idx, c -> Pair(c, 'a' + idx)}.toMap()
+        val converted = words.map { word -> word.map { c -> orderMap[c] }.joinToString("") }
+        val sorted = converted.sorted()
+        println(converted)
+        println(sorted)
+
+        return converted == sorted
+    }
+}
