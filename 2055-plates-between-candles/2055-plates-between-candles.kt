6fe3f1a24a656920bf938bfb8a9130306987daab
Seungwon Min <31559278+zxcv551133@users.noreply.github.com>
2023-02-08 09:13:19 +0900

    Time: 2194 ms (33.33%), Space: 122.1 MB (100.00%) - LeetHub

---
+++ 2055-plates-between-candles/2055-plates-between-candles.kt
@@ -0,0 +1,33 @@
+class Solution {
+    fun platesBetweenCandles(s: String, queries: Array<IntArray>): IntArray {
+        val presumMap = sToPresumMap(s)
+        return queries.map { query ->
+            val left = query[0]
+            val right = query[1]
+            val leftBar = presumMap.ceilingKey(left)
+            val rightBar = presumMap.floorKey(right)
+            println("$left $right $leftBar $rightBar")
+            if(leftBar == null || rightBar == null || leftBar > rightBar) {
+                0
+            } else {
+                presumMap[rightBar]!! - presumMap[leftBar]!!
+            }
+        }.toIntArray()
+    }
+
+    private fun sToPresumMap(s: String): TreeMap<Int, Int> {
+        var currentAcc = 0
+        var lastBar = -1
+        val treeMap = TreeMap<Int, Int>()
+        for(i in s.indices) {
+            if(s[i] == '|') {
+                treeMap[i] = currentAcc
+                lastBar = i
+            } else {
+                currentAcc += 1
+            }
+        }
+        println(treeMap.toList())
+        return treeMap
+    }
+}
