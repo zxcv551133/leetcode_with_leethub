f589f7f9e804d2e88837de92b61b32a194c1572a
Seungwon Min <31559278+zxcv551133@users.noreply.github.com>
2023-02-13 07:30:51 +0900

    Time: 2382 ms (11.11%), Space: 235.4 MB (11.11%) - LeetHub

---
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.kt
@@ -0,0 +1,29 @@
+class Solution {
+    lateinit var fuelGraph: Array<IntArray>
+    fun minimumFuelCost(roads: Array<IntArray>, seats: Int): Long {
+        val graph = Array<MutableList<Int>>(roads.size + 1) { mutableListOf() }
+        for (roadPair in roads) {
+            val a = roadPair[0]
+            val b = roadPair[1]
+            graph[a].add(b)
+            graph[b].add(a)
+        }
+        fuelGraph = graph.map { it.toIntArray() }.toTypedArray()
+
+        return dfs(0, -1, seats).second
+    }
+
+    private fun dfs(currentPos: Int, parentPos: Int, seats: Int): Pair<Int, Long> {
+        val pairSum = fuelGraph[currentPos]
+            .filterNot { it == parentPos }
+            .map { dfs(it, currentPos, seats) }
+            .fold(Pair(1, 0L)) { a, b ->
+                Pair(
+                    a.first + b.first,
+                    a.second + b.second + (b.first + seats - 1) / seats
+                )
+            }
+        println("$pairSum $currentPos $parentPos")
+        return pairSum
+    }
+}
