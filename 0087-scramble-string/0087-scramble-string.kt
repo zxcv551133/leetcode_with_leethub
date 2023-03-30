61472c5b200aa8f23ed9746593c8f1bc7afadca1
Seungwon Min <31559278+zxcv551133@users.noreply.github.com>
2023-03-30 15:14:31 +0900

    Time: 236 ms (90.00%), Space: 37.1 MB (80.00%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.kt
+++ 0087-scramble-string/0087-scramble-string.kt
@@ -1,11 +1,10 @@
 class Solution {
     fun isScramble(s1: String, s2: String): Boolean {
-        val dp = Array(s1.length + 1) { Array(s1.length) { BooleanArray(s1.length) { false } } }
+        val dp = Array(s1.length) { Array(s1.length) { BooleanArray(s1.length + 1) { false } } }
         for (i in s1.indices) {
             for (j in s2.indices) {
-                if(s1[i] == s2[j]) dp[1][i][j] = true
-                // println("$i $j 1 ${dp[1][i][j]}")
-            } 
+                if(s1[i] == s2[j]) dp[i][j][1] = true
+            }
         }
 
         for (len in 2..s1.length) {
@@ -13,19 +12,17 @@ class Solution {
                 for(j in s2.indices) {
                     if(i + len > s1.length || j + len > s2.length) continue
                     for(subLen in 1 until len) {
-                        if((dp[subLen][i][j + len - subLen] && dp[len - subLen][i + subLen][j]) ||
-                            (dp[subLen][i][j] && dp[len - subLen][i + subLen][j + subLen])
+                        if((dp[i][j + len - subLen][subLen] && dp[i + subLen][j][len - subLen]) ||
+                            (dp[i][j][subLen] && dp[i + subLen][j + subLen][len - subLen])
                         ) {
-                            dp[len][i][j] = true
+                            dp[i][j][len] = true
                             continue
                         }
-                        // println("$i $j $len $subLen ${dp[len][i][j]}")
                     }
-                    // println("$i $j $len ${dp[len][i][j]}")
                 }
             }
         }
 
-        return dp[s1.length][0][0]
+        return dp[0][0][s1.length]
     }
 }
