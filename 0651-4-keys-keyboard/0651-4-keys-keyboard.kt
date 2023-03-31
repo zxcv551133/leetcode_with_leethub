c81e4f948ed623f34051bf77d78ec86557a9471b
Seungwon Min <31559278+zxcv551133@users.noreply.github.com>
2023-03-31 22:50:07 +0900

    Time: 140 ms (40.00%), Space: 32.8 MB (60.00%) - LeetHub

--- 0651-4-keys-keyboard/0651-4-keys-keyboard.kt
+++ 0651-4-keys-keyboard/0651-4-keys-keyboard.kt
@@ -1,19 +1,18 @@
 class Solution {
     fun maxA(n: Int): Int {
-        val dp = IntArray( n + 8 + 10 )
-        dp[8] = 1
-        dp[9] = 2
-        dp[10] = 3
-        dp[11] = 4
-        dp[12] = 5
-        for(i in 12..n + 7) {
+        val dp = IntArray( n + 1 + 10 )
+        dp[1] = 1
+        dp[2] = 2
+        dp[3] = 3
+        dp[4] = 4
+        dp[5] = 5
+        for(i in 6..n) {
             var maxVal = dp[i]
             maxVal = kotlin.math.max(maxVal, dp[i - 3] * 2)
             maxVal = kotlin.math.max(maxVal, dp[i - 4] * 3)
             maxVal = kotlin.math.max(maxVal, dp[i - 5] * 4)
             dp[i] = maxVal
-            println(dp[i])
         }
-        return dp[n + 7]
+        return dp[n]
     }
 }
