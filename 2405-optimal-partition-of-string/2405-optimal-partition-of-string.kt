40ed3bdba0e8a62e49817008f5587ef9c88af0cc
Seungwon Min <31559278+zxcv551133@users.noreply.github.com>
2023-04-04 17:21:06 +0900

    Time: 350 ms (7.69%), Space: 41.1 MB (7.69%) - LeetHub

---
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun partitionString(s: String): Int {
+        return s.fold(Pair(mutableSetOf<Char>(), 1)) { (set, cnt), c ->
+            if(set.contains(c)) {
+                // println(set)
+                Pair(mutableSetOf(c), cnt + 1)
+            } else {
+                set.add(c)
+                Pair(set, cnt)
+            }
+        }.second
+    }
+}
