6de82a2849cc46c2dd52484e8fff4adb5aedfff2
Seungwon Min <31559278+zxcv551133@users.noreply.github.com>
2023-04-28 12:45:08 +0900

    Time: 965 ms (100.00%), Space: 288.6 MB (100.00%) - LeetHub

---
+++ 0839-similar-string-groups/0839-similar-string-groups.kt
@@ -0,0 +1,48 @@
+class Solution {
+fun numSimilarGroups(strs: Array<String>): Int {
+        var cnt = 0
+        val visited = BooleanArray(strs.size)
+        val similarGroups = makeSimilarGroups(strs)
+        println(similarGroups.toList())
+        for (i in strs.indices) {
+            if (visited[i]) continue
+            cnt++
+            dfsSimilar(strs, i, visited, similarGroups)
+        }
+
+        return cnt
+    }
+
+    fun makeSimilarGroups(strs: Array<String>): Array<ArrayList<Int>> {
+        val ret = Array(strs.size) { arrayListOf<Int>() }
+
+        for (i in strs.indices) {
+            for (j in strs.indices) {
+                if(i == j) continue
+                if(isSimilar(strs[i], strs[j])) {
+                    ret[i].add(j)
+                }
+            }
+        }
+
+        return ret
+    }
+
+    fun isSimilar(a: String, b: String): Boolean {
+        val diff = arrayListOf<Int>()
+        for(i in a.indices) {
+            if(a[i] == b[i]) continue
+            diff.add(i)
+        }
+        return diff.size == 0 || (diff.size == 2 && a[diff[0]] == b[diff[1]] && a[diff[1]] == b[diff[0]])
+    }
+
+    fun dfsSimilar(strs: Array<String>, currentIdx: Int, visited: BooleanArray, similarGroups: Array<ArrayList<Int>>) {
+        if(visited[currentIdx]) return
+
+        visited[currentIdx] = true
+        for (idx in similarGroups[currentIdx]) {
+            dfsSimilar(strs, idx, visited, similarGroups)
+        }
+    }
+}
