247520fec08a0732f87179d62e4e9e323ea45e82
Seungwon Min <31559278+zxcv551133@users.noreply.github.com>
2023-05-03 17:05:14 +0900

    Time: 507 ms (9.09%), Space: 90.3 MB (9.09%) - LeetHub

---
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.kt
@@ -0,0 +1,5 @@
+class Solution {
+    fun findDifference(nums1: IntArray, nums2: IntArray): List<List<Int>> {
+        return listOf((nums1.toSet() - nums2.toSet()).toList(), (nums2.toSet() - nums1.toSet()).toList())
+    }
+}
