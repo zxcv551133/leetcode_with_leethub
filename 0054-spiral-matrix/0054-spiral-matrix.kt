cc3ed7012863c6178ebcc73ec803abae3513a202
Seungwon Min <31559278+zxcv551133@users.noreply.github.com>
2023-05-09 18:24:13 +0900

    Time: 172 ms (7.18%), Space: 38 MB (5.64%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.kt
+++ 0054-spiral-matrix/0054-spiral-matrix.kt
@@ -14,9 +14,7 @@ class Solution {
 
         fun getNextPoint(current_point: Pair<Int, Int>, direction: Int, origin_direction: Int): Pair<Int, Int> {
             val directionPair = directions[currentDirection]
-            // println("$currentDirection, $directionPair")
             val nextPoint = Pair(stPoint.first + directionPair.first, stPoint.second + directionPair.second)
-            // println("$nextPoint")
             if(matrixCover[nextPoint.first][nextPoint.second] == -300 && direction == origin_direction) {
                 currentDirection = (currentDirection + 1) % 4
                 return getNextPoint(current_point, currentDirection, origin_direction)
@@ -25,7 +23,6 @@ class Solution {
         }
         do {
             stPoint = getNextPoint(stPoint, currentDirection, currentDirection)
-            // println(stPoint)
             val currentVal = matrixCover[stPoint.first][stPoint.second]
             if(currentVal == -300) {
                 break
