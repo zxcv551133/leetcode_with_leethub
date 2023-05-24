d69790a1c6b5c6bc8616a32f6902e4816e0d533d
Seungwon Min <31559278+zxcv551133@users.noreply.github.com>
2023-05-24 14:33:53 +0900

    Time: 1009 ms (60.00%), Space: 175.1 MB (20.00%) - LeetHub

--- 2542-maximum-subsequence-score/2542-maximum-subsequence-score.kt
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.kt
@@ -4,24 +4,17 @@ class Solution {
             .sortedBy { it.second }
             .mapIndexed { idx, (num1, num2) -> Triple(idx.toLong(), num1.toLong(), num2.toLong()) }
         val pq = PriorityQueue<Pair<Long, Long>>(Comparator { a, b -> 
-            // println("$a $b ${b.second.compareTo(a.second)}")
             b.second.compareTo(a.second) })
         val pqIndex = PriorityQueue<Pair<Long, Long>>(Comparator { a, b -> a.first.compareTo(b.first) })
         for(triple in tripleSorted) {
-            // println(triple)
             pq.add(Pair(triple.first, triple.second))
-            // println("${pq.toList()}")
         }
         var currentSum = 0L
         var ret = 0L
-        // println(tripleSorted)
         for (i in 0..(nums1.size - k)) {
             val triple = tripleSorted[i]
             val currentIdx = triple.first
             val currentMin = triple.third
-            // println("$triple")
-            // println("$pqIndex")
-            // println("${pq.toList()}")
 
             while(pqIndex.isNotEmpty() && pqIndex.peek()!!.first < currentIdx) {
                 currentSum -= pqIndex.poll()!!.second
