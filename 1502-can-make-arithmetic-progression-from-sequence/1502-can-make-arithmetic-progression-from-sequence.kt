e2747cf67d47ce9d9f806e8654c02ff380e02e35
Seungwon Min <31559278+zxcv551133@users.noreply.github.com>
2023-06-07 00:19:31 +0900

    Time: 213 ms (11.11%), Space: 38.3 MB (48.15%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.kt
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.kt
@@ -2,10 +2,7 @@ class Solution {
     fun canMakeArithmeticProgression(arr: IntArray): Boolean {
         val sorted = arr.sorted()
         val tailSorted = sorted.drop(1)
-        println(sorted)
-        println(tailSorted)
         val diff = tailSorted.first() - sorted.first()
-        println(diff)
         return sorted.zip(tailSorted).all { (a, b) -> b - a == diff }
     }
 }
