2625897970f25430d4ca32c1e05859d9c8c7e180
Seungwon Min <31559278+zxcv551133@users.noreply.github.com>
2023-07-09 05:04:41 +0900

    Time: 1248 ms (100.00%), Space: 56.2 MB (100.00%) - LeetHub

---
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun putMarbles(weights: IntArray, k: Int): Long {
+        val pairs = weights.zip(weights.drop(1))
+        val sorted = pairs.map { (it.first + it.second).toLong() }.sorted()
+        val minList = sorted.subList(0, k - 1)
+        val maxList = sorted.subList(weights.size - k, weights.size - 1)
+        
+        val firstLastSum = weights.first() + weights.last()
+        
+        val minVal = minList.sum()
+        val maxVal = maxList.sum()
+        
+        return maxVal - minVal
+    }
+}
