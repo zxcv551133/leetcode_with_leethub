b36579073fd7145fd8334f4977b82808bab37c5e
Seungwon Min <31559278+zxcv551133@users.noreply.github.com>
2023-07-10 01:39:43 +0900

    Time: 366 ms (50.00%), Space: 38.9 MB (50.00%) - LeetHub

---
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.kt
@@ -0,0 +1,45 @@
+class Solution {
+    fun largestVariance(s: String): Int {
+        val counts = s.groupBy { it }.mapValues { it.value.size }
+        if (counts.size == 1) return 0
+        if (counts.all { it.value == 1 }) return 0
+
+        var maxVariance = 0
+
+        val dp = Array(26) { IntArray(26) }
+        for (major in 'a'..'z') {
+            for (minor in 'a'..'z') {
+                if (major == minor) continue
+
+                var majorCount = 0
+                var minorCount = 0
+                var restMinorCount = counts[minor] ?: continue
+
+
+                for (character in s) {
+
+                    if (character == major) {
+                        majorCount++
+                    }
+                    if (character == minor) {
+                        minorCount++
+                        restMinorCount--
+                    }
+
+                    if (minorCount > 0) {
+                        maxVariance = kotlin.math.max(maxVariance, majorCount - minorCount)
+                    }
+
+                    if (minorCount > majorCount && restMinorCount > 0) {
+                        minorCount = 0
+                        majorCount = 0
+                    }
+                }
+            }
+        }
+        // println(dp.map { it.toList() })
+
+
+        return maxVariance
+    }
+}
